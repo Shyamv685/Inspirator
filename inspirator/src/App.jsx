a7a21d76576352a43fb4520ebafa846172896e6e
Shyamv685 <shyamkumarv685@gmail.com>
2024-08-20 22:17:26 +0530

    inspirator first commit

---
+++ inspirator/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Index from'./components/Index';
+function App() {
+  return (
+    <>
+      <div>
+     
+<Index/>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ inspirator/src/components/Index.jsx
@@ -0,0 +1,144 @@
+
+import './Index.css';
+
+function App() {
+  return (
+    <>
+      <div className="header">
+     
+        <h1>I N S P I R A  T O R S</h1>
+        <nav>
+            <a href="#">Home</a>  |
+            <a href="#">About</a> |
+            <a href="#">Inventions</a> 
+        </nav>
+   </div>
+   <div>
+    <main>
+        <section>
+           <h1>INSPIRATOR</h1>
+           <p> something that makes someone want to do something or that gives someone an idea about what to do or create.</p>
+           <p>The term "inspirator" generally refers to a person or thing that inspires or motivates others. It’s derived from the Latin word "inspirare," meaning "to breathe into" or "to inspire." An inspirator can be someone who stimulates creativity, provides encouragement, or drives others to achieve their goals. In a broader sense, it can also refer to something that evokes strong feelings or thoughts in people.</p>
+        </section>
+    </main> 
+
+<div>
+  <section>
+  The concept of who the "best" inspirators are can be subjective and depends on individual perspectives and values. However, there are several widely recognized figures known for their inspirational impact across various fields:<br></br>
+These individuals have inspired many through their achievements, personal resilience, and commitment to making a positive impact on the world.<br></br>
+  </section>
+</div>
+
+<div className="card-container">
+    <div className="cards">
+<div className="card1">
+  <img src="./images/Martinimge.jpg"alt="" />
+  <h2>Martin Luther King Jr.</h2>
+  <p> A leader in the American civil rights movement, known for his advocacy of nonviolent protest and his powerful speeches, including the iconic "I Have a Dream" speech.</p>
+  <button>Read More </button>
+</div>
+
+
+<div className="card2">
+  <img src="./images/MahatmaGandhi.jpg"alt="" />
+  <h2>Mahatma Gandhi</h2>
+  <p> Leader of the Indian independence movement, renowned for his philosophy of nonviolence (ahimsa) and civil disobedience.</p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card3">
+  <img src="./images/NelsonMandela.jpg"alt="" />
+  <h2>Nelson Mandela</h2>
+  <p> Anti-apartheid revolutionary and former President of South Africa, admired for his resilience, leadership, and dedication to justice and reconciliation. </p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card4">
+  <img src="./images/MotherTeresa.jpg"alt="" />
+  <h2>Mother Teresa</h2>
+  <p> Catholic nun and missionary, known for her charitable work with the poor and sick in Kolkata, India, and her devotion to helping the marginalized. </p>
+  <button>Read More </button>
+</div>
+
+
+<div className="card5">
+  <img src="./images/SteveJobs.jpg"alt="" />
+  <h2>Steve Jobs</h2>
+  <p> Co-founder of Apple Inc., recognized for his innovative vision and ability to revolutionize technology and design, inspiring creativity and entrepreneurship. </p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card6">
+  <img src="./images/Oprahwin.jpg"alt="" />
+  <h2>Oprah Winfrey</h2>
+  <p> Education activist and Nobel laureate, known for her advocacy of girls' education and her bravery in the face of violence and oppression. </p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card7">
+  <img src="./images/Malalayousafzai.jpg"alt="" />
+  <h2>Malala Yousafzai</h2>
+  <p>  Media mogul, philanthropist, and motivational speaker, influential for her ability to inspire through her personal story and her media platform. </p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card8">
+  <img src="./images/ElonMusk.jpg"alt="" />
+  <h2>Elon Musk</h2>
+  <p>Entrepreneur and innovator, known for his work with Tesla, SpaceX, and other ventures aimed at advancing technology and addressing global challenges. </p>
+  <button>Read More </button>
+</div>
+
+
+
+<div className="card9">
+  <img src="./images/EleanorRoosevelt.jpg"alt="" />
+  <h2>Eleanor Roosevelt</h2>
+  <p>Former First Lady of the United States, a human rights activist and advocate for social justice, known for her work with the United Nations and her role in drafting the Universal Declaration of Human Rights. </p>
+  <button>Read More </button>
+</div>
+
+
+
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+    <footer>
+    <nav>
+            <a href="#">Home</a> |
+            <a href="#">About</a> |
+            <a href="#">Inventions</a>
+        </nav>
+        <p>&copy; 2024 @Inspirators</p>
+    </footer>
+
+
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
